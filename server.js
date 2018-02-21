31df3117bdeb3fd7bdc398a776ad280aecda9814
Amirmahdi Berenjian <amirmahdi74br2@gmail.com>
2018-02-21 12:08:13 +0330

    04 Class exercise

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 2018;
+
+const server = http.createServer((request,response)=>{
+    responses.statusCode = 200;
+    responses.getHeader('Content-Type','text/html');
+    responses.write("<h1 style='color:cyan'>Hello</h1>");
+    responses.end();
+})
+
+server.listen(port,hostname,() => {
+    console.log(`server running at http://${hostname}/${port}/`);
+});
